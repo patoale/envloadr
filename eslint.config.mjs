99705f887544e7ab40913c57bf43500e2730bf7b
Alejandro Pato <dev.patoale@gmail.com>
2025-03-26 12:27:47 +0100

    chore: add initial ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules/*', 'dist/*'],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: false },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+      ],
+      '@typescript-eslint/prefer-regexp-exec': 'off',
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        { allowAny: false, allowNever: true },
+      ],
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
