cb71ea808324af17171e09683192efe9e627e93f
Alejandro Pato <dev.patoale@gmail.com>
2025-05-04 01:22:51 +0200

    feat(cli): define config of cli parser (#75)
    * feat(cli): define a config for flag prefixes
    
    * feat(cli): define a config for flag-value separator
    
    * feat(cli): define a config for option values separator

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
 // ### Environment file
 export const ENV_FILE_COMMENT_PREFIX = '#';
 export const KEY_VALUE_SEPARATOR = '=';
+
+// ### CLI parser
+export const CLI_FLAG_LONG_PREFIX = '--';
+export const CLI_FLAG_SHORT_PREFIX = '-';
+export const CLI_FLAG_VALUE_SEPARATOR = '=';
+export const CLI_OPTION_VALUES_SEPARATOR = ',';
