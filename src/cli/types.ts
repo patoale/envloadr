7cdaa73e5eea0eda3d7be791df04a52df170fbf3
Alejandro Pato <dev.patoale@gmail.com>
2025-05-04 00:18:12 +0200

    feat(cli): define the type for the parser output

--- src/cli/types.ts
+++ src/cli/types.ts
@@ -1,11 +1,15 @@
+export type PrimitiveType = 'boolean' | 'string';
+
+export type OptionSpecType = {
+  isArray: boolean;
+  primitiveType: PrimitiveType;
+};
+
 export interface OptionSpec {
   description: string;
   longFlag: string;
   shortFlag?: string;
-  type?: {
-    isArray: boolean;
-    primitiveType: 'boolean' | 'string';
-  };
+  type?: OptionSpecType;
 }
 
 export type SpecSchema = Record<string, OptionSpec>;
