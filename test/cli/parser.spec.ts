ce392bfa4a815675bb779644f9f0026aeddbb94f
Alejandro Pato <dev.patoale@gmail.com>
2025-05-05 13:01:27 +0200

    test(cli): ensure the command target is part of the input for parsing (#78)

---
+++ test/cli/parser.spec.ts
@@ -0,0 +1,45 @@
+import { parse } from '@/cli/parser';
+
+const schema = {
+  flagA: {
+    description: 'Option A',
+    longFlag: 'flagA',
+    shortFlag: 'fa',
+    type: {
+      isArray: false,
+      primitiveType: 'boolean',
+    },
+  },
+  flagB: {
+    description: 'Option B',
+    longFlag: 'flagB',
+    shortFlag: 'fb',
+    type: {
+      isArray: true,
+      primitiveType: 'string',
+    },
+  },
+  flagC: {
+    description: 'Option C',
+    longFlag: 'flagC',
+    type: {
+      isArray: false,
+      primitiveType: 'string',
+    },
+  },
+  help: {
+    description: 'Help option',
+    longFlag: 'help',
+    shortFlag: 'h',
+  },
+} as const;
+
+describe('parse', () => {
+  it('should throw an error when the input does not contain the target command', () => {
+    const input: string[] = [];
+
+    expect(() => parse(input, schema)).toThrow(
+      'Error parsing CLI input: Missing target command',
+    );
+  });
+});
