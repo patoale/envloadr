0488cb5d63aa389e3db3e604b396ad92d898e6fd
Alejandro Pato <dev.patoale@gmail.com>
2025-05-22 23:58:21 +0200

    feat: define default values for loading environment variables

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,8 @@ export const CLI_FLAG_LONG_PREFIX = '--';
 export const CLI_FLAG_SHORT_PREFIX = '-';
 export const CLI_FLAG_VALUE_SEPARATOR = '=';
 export const CLI_OPTION_VALUES_SEPARATOR = ',';
+
+// ### Default values
+export const DEFAULT_ENV_FILE_PATH = './env';
+export const DEFAULT_OVERRIDE = true;
+export const DEFAULT_VERBOSE = false;
