c3d204cd7a75386720177e7542abcd3029eea36d
Alejandro Pato <dev.patoale@gmail.com>
2025-06-06 19:38:08 +0200

    test(cli): ensure execution stops after help msg printed if help enabled

--- test/cli/index.spec.ts
+++ test/cli/index.spec.ts
@@ -1,6 +1,8 @@
 import { run } from '@/cli';
 import { buildHelp } from '@/cli/help';
 import schema from '@/cli/schema';
+import * as envFile from '@/envFile';
+import childProcess from 'child_process';
 
 describe('run', () => {
   it('should print help message when help option is enabled', () => {
@@ -17,6 +19,27 @@ describe('run', () => {
       expect(console.log).toHaveBeenCalledWith(expectedHelpMessage);
     } finally {
       consoleLogSpy.mockRestore();
+      process.argv = originalArgv;
+    }
+  });
+
+  it('should stop execution after printing help message when help option is enabled', () => {
+    const originalArgv = process.argv;
+    const parseEnvFilesSpy = jest
+      .spyOn(envFile, 'parseEnvFile')
+      .mockImplementation();
+    const spawnSpy = jest.spyOn(childProcess, 'spawn').mockImplementation();
+
+    process.argv = ['node', 'envloadr', '-h', 'node', 'start.js'];
+
+    try {
+      run();
+
+      expect(parseEnvFilesSpy).not.toHaveBeenCalled();
+      expect(spawnSpy).not.toHaveBeenCalled();
+    } finally {
+      spawnSpy.mockRestore();
+      parseEnvFilesSpy.mockRestore();
       process.argv = originalArgv;
     }
   });
