80c678a1c3203d3f4d4f771828517cc509f9a6cf
Alejandro Pato <dev.patoale@gmail.com>
2025-06-06 19:46:19 +0200

    test(cli): ensure execution stops after help msg printed if help enabled (#196)
    * test(cli): ensure execution stops after help msg printed if help enabled
    
    * refactor(cli): simplify process.argv handling

--- test/cli/index.spec.ts
+++ test/cli/index.spec.ts
@@ -1,10 +1,17 @@
 import { run } from '@/cli';
 import { buildHelp } from '@/cli/help';
 import schema from '@/cli/schema';
+import * as envFile from '@/envFile';
+import childProcess from 'child_process';
 
 describe('run', () => {
+  const originalArgv = process.argv;
+
+  afterEach(() => {
+    process.argv = originalArgv;
+  });
+
   it('should print help message when help option is enabled', () => {
-    const originalArgv = process.argv;
     const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
 
     process.argv = ['node', 'envloadr', '-h', 'node', 'start.js'];
@@ -17,7 +24,25 @@ describe('run', () => {
       expect(console.log).toHaveBeenCalledWith(expectedHelpMessage);
     } finally {
       consoleLogSpy.mockRestore();
-      process.argv = originalArgv;
+    }
+  });
+
+  it('should stop execution after printing help message when help option is enabled', () => {
+    const parseEnvFilesSpy = jest
+      .spyOn(envFile, 'parseEnvFile')
+      .mockImplementation();
+    const spawnSpy = jest.spyOn(childProcess, 'spawn').mockImplementation();
+
+    process.argv = ['node', 'envloadr', '-h', 'node', 'start.js'];
+
+    try {
+      run();
+
+      expect(parseEnvFilesSpy).not.toHaveBeenCalled();
+      expect(spawnSpy).not.toHaveBeenCalled();
+    } finally {
+      spawnSpy.mockRestore();
+      parseEnvFilesSpy.mockRestore();
     }
   });
 });
