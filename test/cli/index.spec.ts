d1a56818490e4300860a6a76b1bb6bde303c666e
Alejandro Pato <dev.patoale@gmail.com>
2025-06-07 19:45:07 +0200

    perf(cli): mock parser.parse() function in run() tests

--- test/cli/index.spec.ts
+++ test/cli/index.spec.ts
@@ -1,24 +1,28 @@
 import childProcess from 'child_process';
 import {
-  CLI_FLAG_LONG_PREFIX,
-  CLI_FLAG_VALUE_SEPARATOR,
   DEFAULT_ENV_FILE_PATH,
   DEFAULT_OVERRIDE,
   DEFAULT_VERBOSE,
 } from '@/config';
 import { run } from '@/cli';
 import { buildHelp } from '@/cli/help';
+import * as cliParser from '@/cli/parser';
 import schema from '@/cli/schema';
 import * as envFile from '@/envFile';
+import type { Args, SpecSchema } from '@/cli/parser/types';
 
 describe('run', () => {
-  const originalArgv = process.argv;
+  let cliParseSpy: jest.SpyInstance<
+    Args<SpecSchema>,
+    [input: string[], schema: SpecSchema]
+  >;
   let parseEnvFilesSpy: jest.SpyInstance;
   let spawnSpy: jest.SpyInstance;
 
   beforeAll(() => {
     // Testing these functions is not the objective of this suite,
     // so they can be mocked throughout the process
+    cliParseSpy = jest.spyOn(cliParser, 'parse');
     parseEnvFilesSpy = jest
       .spyOn(envFile, 'parseEnvFiles')
       .mockImplementation();
@@ -26,27 +30,21 @@ describe('run', () => {
   });
 
   afterAll(() => {
+    cliParseSpy.mockRestore();
     parseEnvFilesSpy.mockRestore();
     spawnSpy.mockRestore();
-  });
-
-  afterEach(() => {
-    process.argv = originalArgv;
   });
 
   it('should print help message when help option is enabled', () => {
     const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
 
-    process.argv = [
-      'node',
-      'envloadr',
-      `${CLI_FLAG_LONG_PREFIX}${schema.help.longFlag}`,
-      'node',
-      'start.js',
-    ];
-
     try {
       const expectedHelpMessage = buildHelp(schema);
+
+      cliParseSpy.mockReturnValueOnce({
+        command: { name: 'node', args: ['start.js'] },
+        options: { help: true },
+      });
 
       run();
 
@@ -57,13 +55,10 @@ describe('run', () => {
   });
 
   it('should stop execution after printing help message when help option is enabled', () => {
-    process.argv = [
-      'node',
-      'envloadr',
-      `${CLI_FLAG_LONG_PREFIX}${schema.help.longFlag}`,
-      'node',
-      'start.js',
-    ];
+    cliParseSpy.mockReturnValueOnce({
+      command: { name: 'node', args: ['start.js'] },
+      options: { help: true },
+    });
 
     run();
 
@@ -72,17 +67,13 @@ describe('run', () => {
   });
 
   it(`should load env vars from ${DEFAULT_ENV_FILE_PATH} when the input does not include the file option`, () => {
-    process.argv = [
-      'node',
-      'envloadr',
-      `${CLI_FLAG_LONG_PREFIX}${schema.noOverride.longFlag}`,
-      `${CLI_FLAG_LONG_PREFIX}${schema.verbose.longFlag}`,
-      'node',
-      'start.js',
-    ];
-
     const expectedPathnames = [DEFAULT_ENV_FILE_PATH];
     const expectedOptions = { override: false, verbose: true };
+
+    cliParseSpy.mockReturnValueOnce({
+      command: { name: 'node', args: ['start.js'] },
+      options: { noOverride: true, verbose: true },
+    });
 
     run();
 
@@ -93,17 +84,13 @@ describe('run', () => {
   });
 
   it(`should use override=${DEFAULT_OVERRIDE} when the input does not include the override option`, () => {
-    process.argv = [
-      'node',
-      'envloadr',
-      `${CLI_FLAG_LONG_PREFIX}${schema.files.longFlag}${CLI_FLAG_VALUE_SEPARATOR}.env`,
-      `${CLI_FLAG_LONG_PREFIX}${schema.verbose.longFlag}`,
-      'node',
-      'start.js',
-    ];
-
     const expectedPathnames = ['.env'];
     const expectedOptions = { override: DEFAULT_OVERRIDE, verbose: true };
+
+    cliParseSpy.mockReturnValueOnce({
+      command: { name: 'node', args: ['start.js'] },
+      options: { files: ['.env'], verbose: true },
+    });
 
     run();
 
@@ -114,17 +101,13 @@ describe('run', () => {
   });
 
   it(`should use verbose=${DEFAULT_VERBOSE} when the input does not include the verbose option`, () => {
-    process.argv = [
-      'node',
-      'envloadr',
-      `${CLI_FLAG_LONG_PREFIX}${schema.files.longFlag}${CLI_FLAG_VALUE_SEPARATOR}.env`,
-      `${CLI_FLAG_LONG_PREFIX}${schema.noOverride.longFlag}`,
-      'node',
-      'start.js',
-    ];
-
     const expectedPathnames = ['.env'];
     const expectedOptions = { override: false, verbose: DEFAULT_VERBOSE };
+
+    cliParseSpy.mockReturnValueOnce({
+      command: { name: 'node', args: ['start.js'] },
+      options: { files: ['.env'], noOverride: true },
+    });
 
     run();
 
@@ -135,13 +118,15 @@ describe('run', () => {
   });
 
   it(`should use the default values for options when the input does not contain any options`, () => {
-    process.argv = ['node', 'envloadr', 'node', 'start.js'];
-
     const expectedPathnames = [DEFAULT_ENV_FILE_PATH];
     const expectedOptions = {
       override: DEFAULT_OVERRIDE,
       verbose: DEFAULT_VERBOSE,
     };
+
+    cliParseSpy.mockReturnValueOnce({
+      command: { name: 'node', args: ['start.js'] },
+    });
 
     run();
 
